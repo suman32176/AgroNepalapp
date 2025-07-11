534746264c445f0e216afb34109e993ecdce27b4
Suman Mahara <159681143+suman32176@users.noreply.github.com>
2025-07-11 04:35:52 -0700

    Update settings.py

--- UserAuth/settings.py
+++ UserAuth/settings.py
@@ -93,10 +93,15 @@ WSGI_APPLICATION = 'UserAuth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DB_NAME'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
     }
 }
+
 
 
 # Password validation
