e2179cae32ecf7520af6ed2f1159d4054fa53d93
Suman Mahara <159681143+suman32176@users.noreply.github.com>
2025-07-11 01:06:29 -0700

    Update settings.py

--- UserAuth/settings.py
+++ UserAuth/settings.py
@@ -135,6 +135,7 @@ STATICFILES_DIRS = [
     
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
