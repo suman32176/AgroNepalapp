ceb7d23a4e27772e13c918ba55c6637d24e0258c
Suman Mahara <159681143+suman32176@users.noreply.github.com>
2025-07-11 01:07:20 -0700

    Update settings.py

--- UserAuth/settings.py
+++ UserAuth/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'allauth.account.middleware.AccountMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'UserAuth.urls'
