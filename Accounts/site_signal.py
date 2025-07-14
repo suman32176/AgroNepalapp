e41428f3150593a8aaaf0262c86386fbd5da67d7
Suman Mahara <159681143+suman32176@users.noreply.github.com>
2025-07-14 12:53:30 +0545

    Update site_signal.py

--- Accounts/site_signal.py
+++ Accounts/site_signal.py
@@ -1,12 +1,13 @@
 from django.db.models.signals import post_migrate
-from django.contrib.sites.models import Site
 from django.dispatch import receiver
+from django.contrib.sites.models import Site
 
 @receiver(post_migrate)
 def create_or_update_site(sender, **kwargs):
     Site.objects.update_or_create(
-        domain='agronepal-ozxu.onrender.com',  # Your domain
+        id=1,
         defaults={
+            'domain': 'agronepal-ozxu.onrender.com',
             'name': 'AgroNepal',
         }
     )
